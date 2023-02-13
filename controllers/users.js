931f3d98c3707f701567243773f43943768d8910
kumarakshay57255 <kumarakshay57255@gmail.com>
2023-02-13 13:40:54 +0530

    user login and signUp api message changed

--- controllers/users.js
+++ controllers/users.js
@@ -1,37 +1,66 @@
 const User = require('../models/users');
+const bcrypt = require('bcrypt');
 
-exports.postUsers = async(req,res)=>{
-    try {
-        const {name,email,password}  = req.body;
-        const findEmail = await User.findOne({where:{email:email}});
-        if(findEmail){
-           res.status(200).send('Email already exist');
-           return;
-        }
-
-        const data = await User.create({
-            password,email,name
-        })
-        res.status(200).send('User signUp Successfully');
-    } catch (error) {
-        res.status(500).send(error);
+function isstringinvalid(string){
+    if(string == undefined ||string.length === 0){
+        return true
+    } else {
+        return false
     }
 }
-exports.loginUser  = async (req,res)=>{
-    try {
-        const {email,password} = req.body;
-         const user = await User.findOne({where:{email}});
-         if(!user){
-            return res.status(404);
-         }
-        if(user.dataValues.password === password){
-           return res.status(200).send('User login succesfully');
-        }
-        else{
-            return res.status(401);
-        }
 
+const signUp = async(req,res)=>{
+    try {
+        const {name,email,password}  = req.body;
+        if(isstringinvalid(name) || isstringinvalid(email || isstringinvalid(password))){
+            return res.status(400).json({err: "Bad parameters . Something is missing"})
+        }
+        const findEmail = await User.findOne({where:{email:email}});
+        if(findEmail){
+           res.status(200).json({sucess:true,message:'Email already exist'});
+           return;
+        }
+        const saltrounds = 10;
+        bcrypt.hash(password,saltrounds,async(err,hash)=>{
+            await User.create({name,email,password:hash})
+            res.status(200).json({sucess:true,message:'User signUp Successfully'});
+        })
+
+    
+       
     } catch (error) {
-        res.status(500).send(error);
+        res.status(500).json(error);
     }
+}
+const login = async (req, res) => {
+    try{
+    const { email, password } = req.body;
+    if(isstringinvalid(email) || isstringinvalid(password)){
+        return res.status(400).json({message: 'EMail id or password is missing ', success: false})
+    }
+    console.log(password);
+    const user  = await User.findAll({ where : { email }})
+        if(user.length > 0){
+           bcrypt.compare(password, user[0].password, (err, result) => {
+           if(err){
+            throw new Error('Something went wrong')
+           }
+            if(result === true){
+                return res.status(200).json({success: true, message: "User logged in successfully"})
+            }
+            else{
+            return res.status(400).json({success: false, message: 'Password is incorrect'})
+           }
+        })
+        } else {
+            return res.status(404).json({success: false, message: 'User Doesnot exitst'})
+        }
+    }catch(err){
+        res.status(500).json({ err:err, success: false})
+    }
+}
+
+module.exports={
+    login,
+    signUp
 }
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/users');
 
-router.post('/signUp',userController.postUsers);
-router.post('/login',userController.loginUser);
+router.post('/signUp',userController.signUp);
+router.post('/login',userController.login);
 module.exports = router;
