e03068e2f13466d332ebbc33768c8f31bfc5069e
kumarakshay57255 <kumarakshay57255@gmail.com>
2023-03-02 11:39:21 +0530

    leader board code optimization completed using query

--- controllers/leader.js
+++ controllers/leader.js
@@ -1,33 +1,47 @@
 const User = require('../models/users')
 const Expense = require('../models/expense');
+const sequelize = require('../utils/database');
 
 const showleader = async (req,res)=>{
     try {
-         let users =  await User.findAll();
-         let expense = await Expense.findAll();
-         let usersexpenses = {};
-         expense.map((ele)=>{
-            if(usersexpenses[ele.userId]){
-                usersexpenses[ele.userId] = usersexpenses[ele.userId]+ele.expenseamount;
-            }
-            else{
-                usersexpenses[ele.userId] = ele.expenseamount;
-            }
-           
-         })
-        let userdetailes = [];
-         users.map((user)=>{
-         
-            userdetailes.push({name:user.name,totalcost:usersexpenses[user.id]})
-         })
-     userdetailes.sort((a,b)=>{
-       b.totalcost-a.totalcost;
-     })
+         let users =  await User.findAll({
+            attributes:['id','name',[sequelize.fn('sum',sequelize.col('expenses.expenseamount')),'totalcost']],
+            include:[
+                {
+                    model:Expense,
+                    attributes:[]
+                }
+            ],
+            group:['users.id'],
+            order:[['totalcost','DESC']]
+         });
 
-     
-       return res.status(200).json(userdetailes);
+
+          // Brute force methodss
+    //      let usersexpenses = {};
+    //      expense.map((ele)=>{
+    //         if(usersexpenses[ele.userId]){
+    //             usersexpenses[ele.userId] = usersexpenses[ele.userId]+ele.expenseamount;
+    //         }
+    //         else{
+    //             usersexpenses[ele.userId] = ele.expenseamount;
+    //         }
+           
+    //      })
+    //     let userdetailes = [];
+    //      users.map((user)=>{
+         
+    //         userdetailes.push({name:user.name,totalcost:usersexpenses[user.id]})
+    //      })
+    //  userdetailes.sort((a,b)=>{
+    //    b.totalcost-a.totalcost;
+    //  })
+
+    //  console.log(userdetailes);
+       return res.status(200).json(users);
 
     } catch (error) {
+        console.log(error);
           return res.status(500).json({success:false,err:error});
     }
     }
