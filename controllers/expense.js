4ef684eb23733c1360bdc9aaa59f822c5a3b15f2
kumarakshay57255 <kumarakshay57255@gmail.com>
2023-03-12 23:25:41 +0530

    expense download api created using aws s3

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,7 +1,41 @@
 const Expense = require('../models/expense');
 const User = require('../models/users');
+const AWS  = require('aws-sdk');
 const sequelize = require('../utils/database');
+require('dotenv').config();
 let t;
+
+async function uploadToS3(data,filename){
+    const BUCKET_NAME = 'expensetrac';
+    const IAM_USER_KEY = process.env.AWS_ACCESS_KEY;
+    const IAM_USER_SECRET = process.env.AWS_SECRET_ACCESS_KEY;
+
+    let s3bucket = new AWS.S3({
+        accessKeyId:IAM_USER_KEY,
+        secretAccessKey:IAM_USER_SECRET
+    })
+ 
+        let params = {
+            Bucket:BUCKET_NAME,
+            Key:filename,
+            Body:data,
+            ACL:'public-read'
+        }
+       return new Promise((resolve,reject)=>{
+        s3bucket.upload(params, (err, s3response) => {
+            if (err) {
+                console.log('Something went wrong', err);
+                reject(err);
+            }
+            else {
+                resolve(s3response.Location);
+            }
+        })
+        })
+      
+    
+
+}
 
 const addExpense = async (req,res)=>{
  
@@ -57,11 +91,29 @@ const deleteExpense = async (req,res) =>{
     }
 }
 
+const downloadExpense = async(req,res)=>{
+    try {
+        const {ispremiumuser} = req.user;
+         if(!ispremiumuser){
+            return res.status(401).json({sucess:false,message:"User is not a premium User"})
+         }
+         const expenses = await Expense.findAll();
+         const stringifiedExpenses = JSON.stringify(expenses);
+         const userId = req.user.id;
 
+         const filename = `Expense${userId}/${new Date().toLocaleDateString()}.txt`;
+         const fileURL =  await uploadToS3(stringifiedExpenses,filename);
+         console.log(fileURL)
+          res.status(200).json({fileURL,sucess:true})
+    } catch (error) {
+        
+    }
+}
 
 
 module.exports={
     addExpense,
     getExpense,
-    deleteExpense
+    deleteExpense,
+    downloadExpense
 }
--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/users');
 const userAuthenticate = require('../middlewares/auth');
+const expenseController = require('../controllers/expense');
 
 router.post('/signUp',userController.signUp);
 router.post('/login',userController.login);
-
+router.get('/download', userAuthenticate.authenticate,expenseController.downloadExpense)
 module.exports = router;
