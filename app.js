6a702c34634807aa40ff1b21dc083e1588d96c82
kumarakshay57255 <kumarakshay57255@gmail.com>
2023-03-16 21:35:27 +0530

    npm package installed

--- app.js
+++ app.js
@@ -2,13 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./utils/database')
 const cors = require('cors')
+const helmet  = require('helmet');
+const compression  = require('compression');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 
 const userRoute = require('./routes/user');
 const expenseRoute = require('./routes/expense');
 const purchaseRoute = require('./routes/purchase');
 const resetPasswordRoute = require('./routes/resetpassword');
 const leaderRoute =  require('./routes/leader');
-
+const accessLogStream = fs.createWriteStream(
+    path.join(__dirname,'access.log'),{flags:'a'}
+)
 
 const Users = require('./models/users');
 const Order = require('./models/orders')
@@ -18,6 +25,11 @@ const DownloadExpense = require('./models/downloadexpense');
 const app = express();
 
 
+
+app.use(helmet());
+app.use(compression())
+app.use(morgan('combined',{stream:accessLogStream}))
+
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors())
@@ -26,6 +38,8 @@ app.use('/expense',expenseRoute);
 app.use('/purchase',purchaseRoute);
 app.use('/premium',leaderRoute);
 app.use('/password',resetPasswordRoute);
+
+
 
 Users.hasMany(Expense);
 Expense.belongsTo(Users);
@@ -45,6 +59,6 @@ sequelize.sync({force:false}).then(result=>{
     console.log(err)
 })
   
-
+const PORT = process.env.PORT||4400
   
-  app.listen(4400);
+  app.listen(PORT);
